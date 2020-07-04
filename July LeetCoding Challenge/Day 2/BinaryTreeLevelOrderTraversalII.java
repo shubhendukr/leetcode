9728ac5c7458c1557279ecd2a9560fdb12c09ac8
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2020-07-04 18:25:06 +0800

    Create BinaryTreeLevelOrderTraversalII.java

---
+++ July LeetCoding Challenge/Day 2/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrderBottomList = new LinkedList<List<Integer>>();
+        levelMaker(levelOrderBottomList, root, 0);
+        return levelOrderBottomList;
+    }
+
+    public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+        if(root == null) return;
+        if(level >= list.size()) {
+            list.add(0, new LinkedList<Integer>());
+        }
+        levelMaker(list, root.left, level+1);
+        levelMaker(list, root.right, level+1);
+        list.get(list.size()-level-1).add(root.val);
+    }
+}
