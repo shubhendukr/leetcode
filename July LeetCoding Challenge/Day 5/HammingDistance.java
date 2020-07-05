c75c15376b5e2f005c62686466e7a4adc0be3f08
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2020-07-05 15:49:48 +0800

    Create HammingDistance.java

---
+++ July LeetCoding Challenge/Day 5/HammingDistance.java
@@ -0,0 +1,13 @@
+class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int setBits = 0;
+        
+        while (xor > 0) {
+            setBits += xor & 1;
+            xor >>= 1;
+        }
+        
+        return setBits;
+    }
+}
